0aeb91a59496e7e393bda74fd0b91b1bf59a76e0
Pablo Vizcaino <pvizcaino@PVizcainos-MacBook-Pro.local>
2025-12-05 18:08:37 -0800

    Configure for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/unfamiliar',
+  assetPrefix: '/unfamiliar/',
 };
 
 export default nextConfig;
